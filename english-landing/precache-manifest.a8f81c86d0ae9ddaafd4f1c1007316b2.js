f919b38e7508261542283a5c860e4a9626f136bc
vadim.p <vadimka455@yandex.ru>
2018-11-12 02:35:06 +0300

    add english-landing

---
+++ english-landing/precache-manifest.a8f81c86d0ae9ddaafd4f1c1007316b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ebe3dc368a4d117f0a5d21f063e3c24",
+    "url": "/static/media/12205.0ebe3dc3.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca3945e5b9f64803df4a",
+    "url": "/static/js/main.ca3945e5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ca3945e5b9f64803df4a",
+    "url": "/static/css/main.295b1d00.chunk.css"
+  },
+  {
+    "revision": "0b7e3d106f2b4ed4876fcd49431d68f3",
+    "url": "/index.html"
+  }
+];
