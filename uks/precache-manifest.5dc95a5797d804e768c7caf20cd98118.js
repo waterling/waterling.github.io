05dcc4e286ab63eeb1afeb2e56d7f0f64a10c308
Vadim Plokhotnyuk <v.plohotnyuk@iteco-inno.ru>
2020-02-22 18:05:48 +0300

    Add uks test

---
+++ uks/precache-manifest.5dc95a5797d804e768c7caf20cd98118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7501d1bb3928c579eea3c1b12a7c2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d265d29f0119de92d3f",
+    "url": "/static/css/main.db18a234.chunk.css"
+  },
+  {
+    "revision": "7e33c3efdb1607ede372",
+    "url": "/static/js/2.ef83f716.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.ef83f716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d265d29f0119de92d3f",
+    "url": "/static/js/main.8773437a.chunk.js"
+  },
+  {
+    "revision": "d51a83ede371872f4d6d",
+    "url": "/static/js/runtime-main.486efbef.js"
+  },
+  {
+    "revision": "69ac13d9e9eaed145466d19142150655",
+    "url": "/static/media/logo.69ac13d9.svg"
+  }
+]);
